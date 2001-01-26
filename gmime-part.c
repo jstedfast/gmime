7d370ba5856945cd6ba349c097d1b67c2794ba54
Jeffrey Stedfast <fejj@src.gnome.org>
2001-01-26 03:04:11 +0000

    applied a patch that plugged a leak

--- gmime-part.c
+++ gmime-part.c
@@ -115,6 +115,8 @@ g_mime_part_destroy (GMimePart *mime_part)
 			
 			g_list_free (mime_part->disposition->params);
 		}
+		
+		g_free (mime_part->disposition);
 	}
 	
 	if (mime_part->children) {
@@ -597,7 +599,7 @@ g_mime_part_set_filename (GMimePart *mime_part, const gchar *filename)
  * and if not then checks the "name" parameter in the Content-Type.
  **/
 const gchar *
-g_mime_part_get_filename (GMimePart *mime_part)
+g_mime_part_get_filename (const GMimePart *mime_part)
 {
 	GMimeParam *param;
 	
