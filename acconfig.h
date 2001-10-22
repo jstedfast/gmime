375d57bb6ba2d4eb6d15b5374d8fc0e4a37ba9f5
Jeffrey Stedfast <fejj@src.gnome.org>
2001-10-22 00:26:39 +0000

    more configure.in checks...for stuff like alloca hmmm, unless I did something wrong, my system doesn't have the alloca function? weird...

--- acconfig.h
+++ acconfig.h
@@ -1,15 +1,19 @@
 #undef PACKAGE
 #undef VERSION
 
+#undef _POSIX_SOURCE
+
+#undef STDC_HEADERS
+
+#undef HAVE_STRING_H
+#undef HAVE_STDLIB_H
+#undef HAVE_CTYPE_H
+#undef HAVE_TIME_H
+#undef HAVE_ALLOCA_H
+
 #undef HAVE_TIMEZONE
 #undef HAVE_TM_GMTOFF
 #undef HAVE_ISBLANK
 #undef HAVE_STRFTIME
-#undef hAVE_LOCALTIME
-
-#undef HAVE_CTYPE_H
-#undef HAVE_TIME_H
-
-#ifdef HAVE_ALLOCA_H
-#include <alloca.h>
-#endif
+#undef HAVE_LOCALTIME
+#undef HAVE_ALLOCA
