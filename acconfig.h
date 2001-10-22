2f54c3a913d1ce5b8af06c2e5ab52af674bce74f
Jeffrey Stedfast <fejj@src.gnome.org>
2001-10-22 00:06:45 +0000

    updated acconfig.h

--- acconfig.h
+++ acconfig.h
@@ -4,6 +4,11 @@
 #undef HAVE_TIMEZONE
 #undef HAVE_TM_GMTOFF
 #undef HAVE_ISBLANK
+#undef HAVE_STRFTIME
+#undef hAVE_LOCALTIME
+
+#undef HAVE_CTYPE_H
+#undef HAVE_TIME_H
 
 #ifdef HAVE_ALLOCA_H
 #include <alloca.h>
