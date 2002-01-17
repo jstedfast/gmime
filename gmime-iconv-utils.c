074c476f1ede9db4701a08a3b8c7d41ac7859daa
Jeffrey Stedfast <fejj@ximian.com>
2002-01-17 00:00:20 +0000

    New utility function to iconv n bytes of a string and return a buffer
    2002-01-16  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* gmime-iconv-utils.c (g_mime_iconv_strndup): New utility function
    	to iconv n bytes of a string and return a buffer containing the
    	converted string.
    	(g_mime_iconv_strdup): Same but for the whole string.
    	(g_mime_iconv_locale_to_utf8): Converts a string in the locale
    	charset to utf8.
    	(g_mime_iconv_locale_to_utf8_length): Same but for a sublength of
    	the string.
    	(g_mime_iconv_utf8_to_locale): Converts a string from utf8 to the
    	locale charset.
    	(g_mime_iconv_utf8_to_locale_length): Same but for a sublength of
    	the string.

---
+++ gmime-iconv-utils.c
@@ -0,0 +1,168 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Authors: Jeffrey Stedfast <fejj@ximian.com>
+ *
+ *  Copyright 2002 Ximain, Inc. (www.ximian.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib.h>
+#include <errno.h>
+#include "gmime-iconv-utils.h"
+#include "gmime-charset.h"
+
+
+static gboolean initialized = FALSE;
+
+static iconv_t utf8_to_locale;
+static iconv_t locale_to_utf8;
+
+
+static void
+iconv_utils_init (void)
+{
+	const char *utf8, *locale;
+	
+	g_mime_charset_init ();
+	
+	utf8 = g_mime_charset_name ("utf-8");
+	locale = g_mime_charset_name (g_mime_charset_locale_name ());
+	
+	utf8_to_locale = iconv_open (utf8, locale);
+	locale_to_utf8 = iconv_open (locale, utf8);
+	
+	initialized = TRUE;
+}
+
+char *
+g_mime_iconv_strndup (iconv_t cd, const char *string, size_t n)
+{
+	size_t inleft, outleft, converted = 0;
+	char *out, *outbuf;
+	const char *inbuf;
+	size_t outlen;
+	
+	if (cd == (iconv_t) -1)
+		return g_strndup (string, n);
+	
+	outlen = n * 2 + 16;
+	out = g_malloc (outlen + 1);
+	
+	inbuf = string;
+	inleft = n;
+	
+	do {
+		outbuf = out + converted;
+		outleft = outlen - converted;
+		
+		converted = iconv (cd, &inbuf, &inleft, &outbuf, &outleft);
+		if (converted == (size_t) -1) {
+			if (errno != E2BIG && errno != EINVAL)
+				goto noop;
+		}
+		
+		/*
+		 * E2BIG   There is not sufficient room at *outbuf.
+		 *
+		 * We just need to grow our outbuffer and try again.
+		 */
+		
+		converted = outlen - outleft;
+		if (errno == E2BIG) {
+			outlen += inleft * 2 + 16;
+			out = g_realloc (out, outlen + 1);
+		}
+		
+	} while (errno == E2BIG);
+	
+	/*
+	 * EINVAL  An  incomplete  multibyte sequence has been encoun­
+	 *         tered in the input.
+	 *
+	 * We'll just have to ignore it...
+	 */
+	
+	/* flush the iconv conversion */
+	iconv (cd, NULL, NULL, &outbuf, &outleft);
+	
+	/* reset the cd */
+	iconv (cd, NULL, NULL, NULL, NULL);
+	
+	out[outlen - outleft] = '\0';
+	
+	return out;
+	
+ noop:
+	g_free (out);
+	
+	/* reset the cd */
+	iconv (cd, NULL, NULL, NULL, NULL);
+	
+	return g_strndup (string, n);
+}
+
+
+char *
+g_mime_iconv_strdup (iconv_t cd, const char *string)
+{
+	return g_mime_iconv_strndup (cd, string, strlen (string));
+}
+
+
+char *
+g_mime_iconv_locale_to_utf8 (const char *string)
+{
+	if (!initialized)
+		iconv_utils_init ();
+	
+	return g_mime_iconv_strdup (locale_to_utf8, string);
+}
+
+
+char *
+g_mime_iconv_locale_to_utf8_length (const char *string, size_t n)
+{
+	if (!initialized)
+		iconv_utils_init ();
+	
+	return g_mime_iconv_strndup (locale_to_utf8, string, n);
+}
+
+
+char *
+g_mime_iconv_utf8_to_locale (const char *string)
+{
+	if (!initialized)
+		iconv_utils_init ();
+	
+	return g_mime_iconv_strdup (utf8_to_locale, string);
+}
+
+
+char *
+g_mime_iconv_utf8_to_locale_length (const char *string, size_t n)
+{
+	if (!initialized)
+		iconv_utils_init ();
+	
+	return g_mime_iconv_strndup (utf8_to_locale, string, n);
+}
---
+++ gmime/gmime-iconv-utils.h
@@ -0,0 +1,48 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *  Authors: Jeffrey Stedfast <fejj@ximian.com>
+ *
+ *  Copyright 2002 Ximain, Inc. (www.ximian.com)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
+#ifndef __GMIME_ICONV_UTILS_H__
+#define __GMIME_ICONV_UTILS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#pragma }
+#endif /* __cplusplus */
+
+#include <sys/types.h>
+#include <iconv.h>
+
+char *g_mime_iconv_strdup (iconv_t cd, const char *string);
+char *g_mime_iconv_strndup (iconv_t cd, const char *string, size_t n);
+
+char *g_mime_iconv_locale_to_utf8 (const char *string);
+char *g_mime_iconv_locale_to_utf8_length (const char *string, size_t n);
+
+char *g_mime_iconv_utf8_to_locale (const char *string);
+char *g_mime_iconv_utf8_to_locale_length (const char *string, size_t n);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __GMIME_ICONV_UTILS_H__ */
