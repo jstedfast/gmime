a35797cb6009cad603857b8836f81b7123d94b45
Jeffrey Stedfast <fejj@ximian.com>
2002-04-14 05:25:40 +0000

    Check for off_t, size_t, and ssize_t.
    2002-04-14  Jeffrey Stedfast  <fejj@ximian.com>
    
    	* configure.in: Check for off_t, size_t, and ssize_t.

--- acconfig.h
+++ acconfig.h
@@ -1,51 +1,8 @@
-#undef PACKAGE
-#undef VERSION
-
-#undef _POSIX_SOURCE
-
-#undef STDC_HEADERS
-
-/* needed headers */
-#undef HAVE_SYS_MMAN_H
-#undef HAVE_ALLOCA_H
-#undef HAVE_STRING_H
-#undef HAVE_STDLIB_H
-#undef HAVE_CTYPE_H
-#undef HAVE_TIME_H
-
-/* needed functions */
+/* Define if your system defines timezone in <time.h>. */
 #undef HAVE_TIMEZONE
+
+/* Define if your system has `tm_gmtoff' in `struct tm'. */
 #undef HAVE_TM_GMTOFF
-#undef HAVE_ISBLANK
-#undef HAVE_STRFTIME
-#undef HAVE_LOCALTIME
-#undef HAVE_ALLOCA
 
-#undef HAVE_ICONV
-#undef ICONV_CONST
+/* Define to use auto-detected iconv-friendly charset names. */
 #undef HAVE_ICONV_DETECT_H
-
-#undef HAVE_MMAP
-#undef HAVE_MUNMAP
-#undef HAVE_MSYNC
-
-/* strlib functions */
-#undef HAVE_MEMCHR
-#undef HAVE_MEMRCHR
-#undef HAVE_MEMMEM
-#undef HAVE_STRLEN
-#undef HAVE_STRCPY
-#undef HAVE_STRNCPY
-#undef HAVE_STRLCPY
-#undef HAVE_STPCPY
-#undef HAVE_STRCAT
-#undef HAVE_STRNCAT
-#undef HAVE_STRLCAT
-#undef HAVE_STRCHR
-#undef HAVE_STRRCHR
-#undef HAVE_STRNSTR
-#undef HAVE_STRSTR
-#undef HAVE_STRNCASESTR
-#undef HAVE_STRCASESTR
-#undef HAVE_STRNCASECMP
-#undef HAVE_STRCASECMP
