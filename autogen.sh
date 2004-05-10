0f3800b273ecdcbc6fe9694d790b3716c9b8aa33
Jeffrey Stedfast <fejj@src.gnome.org>
2004-05-10 00:23:26 +0000

    updated

--- autogen.sh
+++ autogen.sh
@@ -4,62 +4,88 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-THEDIR=`pwd`
+ORIGDIR=`pwd`
 cd $srcdir
+PROJECT=GMime
+TEST_TYPE=-f
+FILE=configure.in
+
 DIE=0
+
+have_libtool=false
+if libtoolize --version < /dev/null > /dev/null 2>&1 ; then
+	libtool_version=`libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+	case $libtool_version in
+	    1.4*|1.5*)
+		have_libtool=true
+		;;
+	esac
+fi
+if $have_libtool ; then : ; else
+	echo
+	echo "You must have libtool 1.4 installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/libtool/"
+	DIE=1
+fi
+
+(gtkdocize --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have gtk-doc installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnome.org/pub/GNOME/sources/gtk-doc/"
+	DIE=1
+}
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to compile gmime."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
 	DIE=1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
+if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
+    AUTOMAKE=automake-1.7
+    ACLOCAL=aclocal-1.7
+else
 	echo
-	echo "You must have libtool installed to compile gmime."
-	echo "Get ftp://alpha.gnu.org/gnu/libtool-1.0h.tar.gz"
-	echo "(or a newer version if it is available)"
+	echo "You must have automake 1.7.x installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
 	DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile gmime."
-	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
+fi
 
 if test "$DIE" -eq 1; then
 	exit 1
 fi
 
-test -f configure.in || {
-	echo "You must run this script in the top-level gmime directory"
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
 	exit 1
 }
 
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+if test -z "$AUTOGEN_SUBDIR_MODE"; then
+        if test -z "$*"; then
+                echo "I am going to run ./configure with no arguments - if you wish "
+                echo "to pass any to it, please specify them on the $0 command line."
+        fi
 fi
 
-autoheader
-libtoolize --copy --force
-aclocal $ACLOCAL_FLAGS
-automake --add-missing
-autoconf
+$ACLOCAL $ACLOCAL_FLAGS || exit $?
 
-cd $THEDIR
+libtoolize --force || exit $?
+gtkdocize || exit $?
 
-if test x$OBJ_DIR != x; then
-    mkdir -p "$OBJ_DIR"
-    cd "$OBJ_DIR"
+autoheader || exit $?
+
+$AUTOMAKE --add-missing || exit $?
+autoconf || exit $?
+cd $ORIGDIR || exit $?
+
+if test -z "$AUTOGEN_SUBDIR_MODE"; then
+        $srcdir/configure --enable-maintainer-mode $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
+
+        echo 
+        echo "Now type 'make' to compile $PROJECT."
 fi
-
-$srcdir/configure "$@"
-
-echo 
-echo "Now type 'make' to compile gmime."
