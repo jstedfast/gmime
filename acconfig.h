79a356f6ad2233085e08d274f77f3ca6914d65bf
Jeffrey Stedfast <fejj@src.gnome.org>
2007-02-10 05:55:47 +0000

    removed acconfig.h
    svn path=/trunk/; revision=1081

--- acconfig.h
+++
@@ -1,8 +0,0 @@
-/* Define to use auto-detected iconv-friendly charset names. */
-#undef HAVE_ICONV_DETECT_H
-
-/* Define if you have fsync() */
-#undef HAVE_FSYNC
-
-/* Define if your sys/param.h does not define MAXHOSTNAMELEN */
-#undef MAXHOSTNAMELEN
