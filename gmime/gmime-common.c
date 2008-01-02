a1bba8c19a15987265b41b816e5696773fece546
Jeffrey Stedfast <fejj@src.gnome.org>
2008-01-02 22:32:25 +0000

    updated copyright notice for 2008
    svn path=/trunk/; revision=1199

--- gmime/gmime-common.c
+++ gmime/gmime-common.c
@@ -1,8 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-/*
- *  Authors: Jeffrey Stedfast <fejj@ximian.com>
- *
- *  Copyright 2003-2004 Ximian, Inc. (www.ximian.com)
+/*  GMime
+ *  Copyright (C) 2000-2008 Jeffrey Stedfast
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
@@ -18,7 +16,6 @@
  *  License along with this library; if not, write to the Free
  *  Software Foundation, 51 Franklin Street, Fifth Floor, Boston, MA
  *  02110-1301, USA.
- *
  */
 
 
