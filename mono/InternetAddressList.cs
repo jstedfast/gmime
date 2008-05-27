66b8fe05c7e32b6ca3245fcd860949bda1781f21
Jeffrey Stedfast <fejj@src.gnome.org>
2008-05-27 16:57:52 +0000

    cleanup
    svn path=/trunk/; revision=1287

--- mono/InternetAddressList.cs
+++ mono/InternetAddressList.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Runtime.InteropServices;
 
 namespace GMime {
-
 	public class InternetAddressList : ArrayList, IDisposable {
 		private IntPtr raw_list;
 		private bool needs_destroy;
@@ -18,19 +17,19 @@ namespace GMime {
 		static extern void internet_address_list_destroy (IntPtr raw);
 
 		public InternetAddressList (IntPtr raw_list, bool needs_destroy)
-			{
-				this.raw_list = raw_list;
-				this.needs_destroy = needs_destroy;
+		{
+			this.raw_list = raw_list;
+			this.needs_destroy = needs_destroy;
+			
+			while (raw_list != IntPtr.Zero) {
+				IntPtr raw_ia = internet_address_list_get_address (raw_list);
 				
-				while (raw_list != IntPtr.Zero) {
-					IntPtr raw_ia = internet_address_list_get_address (raw_list);
-					
-					InternetAddress addr = new InternetAddress (raw_ia);
-					this.Add (addr);
-					
-					raw_list = internet_address_list_next (raw_list);
-				}
+				InternetAddress addr = new InternetAddress (raw_ia);
+				this.Add (addr);
+				
+				raw_list = internet_address_list_next (raw_list);
 			}
+		}
 
 		public InternetAddressList (IntPtr raw_list) : this (raw_list, false) { }
 
