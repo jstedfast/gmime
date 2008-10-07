d73090a7498d376cccc9c6642b5d936133ff968c
Jeffrey Stedfast <fejj@src.gnome.org>
2008-10-07 16:18:25 +0000

    oops, add prototype to header
    svn path=/trunk/; revision=1496

--- util/gtrie.h
+++ util/gtrie.h
@@ -33,6 +33,8 @@ void g_trie_free (GTrie *trie);
 
 void g_trie_add (GTrie *trie, const char *pattern, int pattern_id);
 
+const char *g_trie_quick_search (GTrie *trie, const char *buffer, size_t buflen, int *matched_id);
+
 const char *g_trie_search (GTrie *trie, const char *buffer, size_t buflen, int *matched_id);
 
 G_END_DECLS
