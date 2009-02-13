9c5b61ae8be19a86664af73d1f55a104a72db012
Jeffrey Stedfast <fejj@src.gnome.org>
2009-02-13 04:26:36 +0000

    updated copyrights
    svn path=/trunk/; revision=1505

--- gmime/gmime-parse-utils.h
+++ gmime/gmime-parse-utils.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-/*
- *  Copyright (C) 2000-2008 Jeffrey Stedfast
+/*  GMime
+ *  Copyright (C) 2000-2009 Jeffrey Stedfast
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
