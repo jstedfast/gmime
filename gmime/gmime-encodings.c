65292a0adae7d37bf3a1b147c2984270e015d32f
Jeffrey Stedfast <fejj@gnome.org>
2009-10-10 11:12:17 -0400

    Fixed bug #597794
    2009-10-10  Jeffrey Stedfast  <fejj@novell.com>
    
    	* gmime/gmime-encodings.c (g_mime_encoding_uudecode_step): Fixed a
    	uudecoding bug when uulen % 3 is 2. Thanks to Peter Hartley. Fixes
    	bug #597794.

--- gmime/gmime-encodings.c
+++ gmime/gmime-encodings.c
@@ -830,7 +830,8 @@ g_mime_encoding_uudecode_step (const unsigned char *inbuf, size_t inlen, unsigne
 						*outptr++ = gmime_uu_rank[b0] << 2 | gmime_uu_rank[b1] >> 4;
 						uulen--;
 					}
-					if (uulen >= 2) {
+					
+					if (uulen >= 1) {
 						*outptr++ = gmime_uu_rank[b1] << 4 | gmime_uu_rank[b2] >> 2;
 						uulen--;
 					}
