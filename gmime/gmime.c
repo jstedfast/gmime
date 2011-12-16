a0fa99d917374cd3fef02a5cd4a3ed65aae66382
Jeffrey Stedfast <fejj@gnome.org>
2011-12-15 22:24:41 -0500

    Finer-grained ENABLE_CRYPTO initialization

--- gmime/gmime.c
+++ gmime/gmime.c
@@ -109,8 +109,8 @@ g_mime_init (guint32 flags)
 	gmime_error_quark = g_quark_from_static_string ("gmime");
 	
 	/* register our GObject types with the GType system */
-#ifdef ENABLE_CRYPTOGRAPHY
 	g_mime_cipher_context_get_type ();
+#ifdef ENABLE_CRYPTOGRAPHY
 	g_mime_gpg_context_get_type ();
 #endif
 	
