5d357ec264e243fa5c3e0fb92c447d715a6c1452
Jeffrey Stedfast <fejj@gnome.org>
2011-12-15 21:58:16 -0500

    Don't init crypto unless crypt was enabled
    2011-12-15  Jeffrey Stedfast  <fejj@gnome.org>
    
    	* gmime/gmime.c (g_mime_init): Don't initialize the crypto types
    	unless crypto was enabled in the build.

--- gmime/gmime.c
+++ gmime/gmime.c
@@ -120,6 +120,7 @@ g_mime_init (guint32 flags)
 	gmime_error_quark = g_quark_from_static_string ("gmime");
 	
 	/* register our GObject types with the GType system */
+#ifdef ENABLE_CRYPTOGRAPHY
 	g_mime_crypto_context_get_type ();
 	g_mime_decrypt_result_get_type ();
 	g_mime_certificate_list_get_type ();
@@ -128,6 +129,7 @@ g_mime_init (guint32 flags)
 	g_mime_signature_get_type ();
 	g_mime_gpg_context_get_type ();
 	g_mime_pkcs7_context_get_type ();
+#endif
 	
 	g_mime_filter_get_type ();
 	g_mime_filter_basic_get_type ();
