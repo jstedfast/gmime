5d4e079c2ad197563a3e900573f9cfc80c42fa4c
Jeffrey Stedfast <fejj@gnome.org>
2012-01-08 12:13:46 -0500

    Bumped version to 2.6.4
    2012-01-08  Jeffrey Stedfast  <fejj@gnome.org>
    
    	* README: Bumped version
    
    	* configure.ac: Bumped version to 2.6.4

--- tests/test-pgpmime.c
+++ tests/test-pgpmime.c
@@ -433,7 +433,7 @@ int main (int argc, char *argv[])
 	}
 	
 	if (i < argc && (stat (datadir, &st) == -1 || !S_ISDIR (st.st_mode)))
-		return EXIT_FAILURE;
+		return 0;
 	
 	testsuite_start ("PGP/MIME implementation");
 	
