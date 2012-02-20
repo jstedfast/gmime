55cda55107116638d26ae88f1faf72cbb1d615cc
Jeffrey Stedfast <fejj@gnome.org>
2012-02-19 19:54:58 -0500

    Handle non-standard Content-Transfer-Encodings names
    2012-02-19  Jeffrey Stedfast  <fejj@gnome.org>
    
    	* gmime/gmime-encodings.c (g_mime_content_encoding_from_string):
    	Match non-standard encodings: 7-bit, 8-bit, and x-uue (the
    	standard names for these are 7bit, 8bit, and x-uuencode).

--- gmime/gmime-encodings.c
+++ gmime/gmime-encodings.c
@@ -117,6 +117,10 @@ g_mime_content_encoding_from_string (const char *str)
 		return GMIME_CONTENT_ENCODING_7BIT;
 	else if (!g_ascii_strcasecmp (str, "8bit"))
 		return GMIME_CONTENT_ENCODING_8BIT;
+	else if (!g_ascii_strcasecmp (str, "7-bit"))
+		return GMIME_CONTENT_ENCODING_7BIT;
+	else if (!g_ascii_strcasecmp (str, "8-bit"))
+		return GMIME_CONTENT_ENCODING_8BIT;
 	else if (!g_ascii_strcasecmp (str, "binary"))
 		return GMIME_CONTENT_ENCODING_BINARY;
 	else if (!g_ascii_strcasecmp (str, "base64"))
@@ -126,6 +130,8 @@ g_mime_content_encoding_from_string (const char *str)
 	else if (!g_ascii_strcasecmp (str, "uuencode"))
 		return GMIME_CONTENT_ENCODING_UUENCODE;
 	else if (!g_ascii_strcasecmp (str, "x-uuencode"))
+		return GMIME_CONTENT_ENCODING_UUENCODE;
+	else if (!g_ascii_strcasecmp (str, "x-uue"))
 		return GMIME_CONTENT_ENCODING_UUENCODE;
 	else
 		return GMIME_CONTENT_ENCODING_DEFAULT;
