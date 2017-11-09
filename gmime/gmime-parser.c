a1786b979ab675d0f39d421478b6385b7832d9c4
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-11-09 15:37:31 -0500

    style fixes

--- gmime/gmime-parser.c
+++ gmime/gmime-parser.c
@@ -872,11 +872,13 @@ next_alloc_size (size_t n)
 static inline gboolean
 is_7bit_clean (const gchar *str)
 {
-	for (; *str != '\0'; str++) {
-		if ((*str & 0x80) != 0) {
+	while (*str != '\0') {
+		if ((*str & 0x80) != 0)
 			return FALSE;
-		}
+		
+		str++;
 	}
+	
 	return TRUE;
 }
 
@@ -942,7 +944,7 @@ header_parse (GMimeParser *parser, GMimeParserOptions *options)
 		priv->header_cb (parser, header->name, header->raw_value,
 				 header->offset, priv->user_data);
 	
-	if (can_warn && (!is_7bit_clean(header->name) || !g_utf8_validate (header->raw_value, -1, NULL)))
+	if (can_warn && (!is_7bit_clean (header->name) || !g_utf8_validate (header->raw_value, -1, NULL)))
 		_g_mime_parser_options_warn (options, header->offset, GMIME_WARN_UNENCODED_8BIT_HEADER, header->name);
 }
 
