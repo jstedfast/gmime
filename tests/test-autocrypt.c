6cbb418228b6b2bc83d5fcadba174d7ddc9bb577
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-12-13 07:14:48 -0500

    Disable verbose debug output for autocrypt tests

--- tests/test-autocrypt.c
+++ tests/test-autocrypt.c
@@ -1052,8 +1052,6 @@ int main (int argc, char **argv)
 	
 	testsuite_init (argc, argv);
 	
-	verbose = 4;
-	
 	if (!(gpg = g_find_program_in_path ("gpg2")))
 		if (!(gpg = g_find_program_in_path ("gpg")))
 			return EXIT_FAILURE;
