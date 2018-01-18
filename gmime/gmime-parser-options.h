jstedfast/gmime

commit 14671817dc1a64d8840b2f612cd143f29d55ecbf

Report invalid message header parameters (#41)
The warning is triggered when a parameter value is missing, the parameter name
or value are malformed, or the parameter list ends with a ';'.  This is
typically not critical, but may indicate a spam message.

gmime/gmime-parser-options.h
@@ -48,6 +48,7 @@ typedef enum {
  * @GMIME_WARN_UNENCODED_8BIT_HEADER: a header contains unencoded 8-bit characters
  * @GMIME_WARN_INVALID_CONTENT_TYPE: invalid content type, assume `application/octet-stream`
  * @GMIME_WARN_INVALID_RFC2047_HEADER_VALUE: invalid RFC 2047 encoded header value
+ * @GMIME_WARN_INVALID_PARAMETER: invalid header parameter
  * @GMIME_WARN_MALFORMED_MULTIPART: no items in a `multipart/...`
  * @GMIME_WARN_TRUNCATED_MESSAGE: the message is truncated
  * @GMIME_WARN_MALFORMED_MESSAGE: the message is malformed
@@ -65,6 +66,7 @@ typedef enum {
 	GMIME_WARN_UNENCODED_8BIT_HEADER,
 	GMIME_WARN_INVALID_CONTENT_TYPE,
 	GMIME_WARN_INVALID_RFC2047_HEADER_VALUE,
+	GMIME_WARN_INVALID_PARAMETER,
 	GMIME_WARN_MALFORMED_MULTIPART,
 	GMIME_WARN_TRUNCATED_MESSAGE,
 	GMIME_WARN_MALFORMED_MESSAGE,

examples/msgcheck.c
@@ -44,6 +44,8 @@ errcode2str(GMimeParserWarning errcode)
 		return "invalid Content-Type";
 	case GMIME_WARN_INVALID_RFC2047_HEADER_VALUE:
 		return "invalid RFC 2047 encoded header value";
+	case GMIME_WARN_INVALID_PARAMETER:
+		return "invalid header parameter";
 	case GMIME_WARN_MALFORMED_MULTIPART:
 		return "malformed multipart";
 	case GMIME_WARN_TRUNCATED_MESSAGE:

gmime/gmime-param.c
@@ -1108,7 +1108,8 @@ decode_param (GMimeParserOptions *options, const char **in, char **namep, char *
 		*namep = name;
 		*in = inptr;
 		return TRUE;
-	}
+	} else
+		_g_mime_parser_options_warn (options, offset, GMIME_WARN_INVALID_PARAMETER, name);
 	
 	g_free (value);
 	g_free (name);

commit a427ed16dac05de2fc7a825dd25b8f740133b5e2

Avoid API breakage of GMimeParserWarning
in 14671817dc1a64d8840b2f612cd143f29d55ecbf,
GMIME_WARN_INVALID_PARAMETER was introduced, but it also changed the
enum values for every GMimeParserWarning that followed it in the list.

This means that if someone built against a prior version of GMime and
then upgraded their library, the library might try to return, say,
GMIME_CRIT_CONFLICTING_HEADER but the application would interpret it
as GMIME_CRIT_CONFLICTING_PARAMETER.

This change restores the original API for GMimeParserWarning, since
there is probably already released and distributed software built
against the older header files.

gmime/gmime-parser-options.h
@@ -66,14 +66,14 @@ typedef enum {
 	GMIME_WARN_UNENCODED_8BIT_HEADER,
 	GMIME_WARN_INVALID_CONTENT_TYPE,
 	GMIME_WARN_INVALID_RFC2047_HEADER_VALUE,
-	GMIME_WARN_INVALID_PARAMETER,
 	GMIME_WARN_MALFORMED_MULTIPART,
 	GMIME_WARN_TRUNCATED_MESSAGE,
 	GMIME_WARN_MALFORMED_MESSAGE,
 	GMIME_CRIT_INVALID_HEADER_NAME,
 	GMIME_CRIT_CONFLICTING_HEADER,
 	GMIME_CRIT_CONFLICTING_PARAMETER,
-	GMIME_CRIT_MULTIPART_WITHOUT_BOUNDARY
+	GMIME_CRIT_MULTIPART_WITHOUT_BOUNDARY,
+	GMIME_WARN_INVALID_PARAMETER
 } GMimeParserWarning;
 
 /**