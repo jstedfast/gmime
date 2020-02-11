aac1302b092a48089083c79fc4a686ac0662d658
Vitaliy Didik <ariman@inbox.ru>
2020-02-11 18:23:09 +0300

    Added MSVC project for 'msgcheck' example program.

--- examples/msgcheck.c
+++ examples/msgcheck.c
@@ -19,6 +19,10 @@
  *  Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <stdlib.h>
 #include <gmime/gmime.h>
 
