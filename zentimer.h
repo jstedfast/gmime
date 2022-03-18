e8a35106f08a2debc620f70efb07afbdd248a424
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-03-18 16:44:48 -0400

    Updated copyright

--- zentimer.h
+++ zentimer.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*  ZenTimer
- *  Copyright (C) 2000-2020 Jeffrey Stedfast
+ *  Copyright (C) 2000-2022 Jeffrey Stedfast
  *
  *  Permission is hereby granted, free of charge, to any person
  *  obtaining a copy of this software and associated documentation
